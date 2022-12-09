4937e9a6ae04313a7746f01ba060e9ef20a534cb
lookatsampaio <lookatsampaio@gmail.com>
2022-12-09 02:57:33 -0300

    Pronto

--- Lista (1)/atividade1.c
+++ Lista (1)/atividade1.c
@@ -1,1 +1,16 @@
-//oioi
+#include <stdio.h>
+
+int main (){
+
+int horas, segundos, minutos;
+
+printf ("Digite as horas\n");
+scanf ("%d", &horas);
+fflush(stdin);
+
+minutos = horas * 60;
+segundos = minutos * 60;
+
+printf ("\n\n\n%dh equivale(m):\n\nEM MINUTOS\n%dmin\nEM SEGUNDOS\n%ds", horas, minutos, segundos);
+
+}
