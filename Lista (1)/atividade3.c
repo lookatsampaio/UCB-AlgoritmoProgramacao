5967ab01b3ebcb0789d2fa5cf5ea9ca97cb3e63b
lookatsampaio <lookatsampaio@gmail.com>
2022-12-09 03:22:21 -0300

    atividade3 feita

---
+++ Lista (1)/atividade3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (){
+
+float primeiroNum, segundoNum, terceiroNum, resultado;
+
+printf ("Digite o primeiro numero:\n");
+scanf ("%f", &primeiroNum);
+fflush(stdin);
+
+printf ("\nDigite o segundo numero:\n");
+scanf ("%f", &segundoNum);
+fflush(stdin);
+
+printf ("\nDigite o terceiro numero:\n");
+scanf ("%f", &terceiroNum);
+fflush(stdin);
+
+resultado = (primeiroNum + segundoNum + terceiroNum) / 3;
+
+printf ("Media Aritmetica = %.2f", resultado);
+
+}
