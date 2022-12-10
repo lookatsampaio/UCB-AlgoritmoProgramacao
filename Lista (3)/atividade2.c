5800bbb78cd43dec07197c8f73e483b26b5a3fb0
lookatsampaio <lookatsampaio@gmail.com>
2022-12-10 18:02:18 -0300

    atividades1,2,3 feitas

--- Lista (3)/atividade2.c
+++ Lista (3)/atividade2.c
@@ -5,13 +5,16 @@ int main () {
 
     printf("Digite o primeiro numero: ");
     scanf("%d", &num1);
-
     printf("Digite o segundo numero: ");
     scanf("%d", &num2);
 
     if (num1 > num2) {
         printf("O primeiro numero e maior que o segundo");
-    } else {
-        printf("O primeiro numero e menor que o segundo");
+    }
+    else if (num1 < num2) {
+        printf("O segundo numero e maior que o primeiro");
+    }
+    else {
+        printf("Os dois numeros sao iguais");
     }
 }
